777fb3d80db4c43ff7be09202999a64052c718ef
ha-pu <66355167+ha-pu@users.noreply.github.com>
2023-04-11 20:38:53 -0400

    Update 2018

--- code/0_parameters.r
+++ code/0_parameters.r
@@ -11,5 +11,5 @@ if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("WDI")) install.packages("WDI")
 
 # save new year ----------------------------------------------------------------
-year <- 2017
+year <- 2018
 write_lines(year, "input/new_year.txt")
