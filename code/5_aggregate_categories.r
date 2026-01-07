0601a6ce69b57e0e24bf9df21e247327738cb9bb
ha-pu <66355167+ha-pu@users.noreply.github.com>
2026-01-07 08:38:39 +0100

    Update code base

--- code/5_aggregate_categories.r
+++ code/5_aggregate_categories.r
@@ -16,33 +16,40 @@ data_wdi <- read_rds("data/data_wdi.rds")
 # compute category -------------------------------------------------------------
 category_base <- data_keyword %>%
   filter(spri > 0) %>%
-  group_by(location, date, category) %>%
   summarise(
     spri = mean(spri),
-    .groups = "drop"
+    .by = c(location, date, category)
   ) %>%
   mutate(year = year(date))
 
 category_global <- category_base %>%
   left_join(data_wdi, by = c("location" = "iso2c", "year")) %>%
   filter(spri != 0 & !is.na(gdp_share)) %>%
-  group_by(date, category) %>%
-  mutate(gdp_total = sum(gdp_share)) %>%
+  mutate(
+    gdp_total = sum(gdp_share),
+    .by = c(date, category)
+  ) %>%
   mutate(gdp_share = gdp_share / gdp_total) %>%
   mutate(spri = spri * gdp_share) %>%
-  group_by(date, category, year) %>%
-  summarise(spri = sum(spri), .groups = "drop")
+  summarise(
+    spri = sum(spri),
+    .by = c(date, category, year)
+  )
 
 category_internet <- category_base %>%
   left_join(data_wdi, by = c("location" = "iso2c", "year")) %>%
   mutate(internet_share = internet_users * population_share) %>%
   filter(spri != 0 & !is.na(internet_share)) %>%
-  group_by(date, category) %>%
-  mutate(internet_total = sum(internet_share)) %>%
+  mutate(
+    internet_total = sum(internet_share),
+    .by = c(date, category)
+  ) %>%
   mutate(internet_share = internet_share / internet_total) %>%
   mutate(spri = spri * internet_share) %>%
-  group_by(date, category, year) %>%
-  summarise(spri = sum(spri), .groups = "drop")
+  summarise(
+    spri = sum(spri),
+    .by = c(date, category, year)
+  )
 
 # save data --------------------------------------------------------------------
 saveRDS(category_base, "data/category_base.rds")
